7f2b06e24ca3323fe511e39feb99d1a0294cc828
H153165g <chrn22018@g.nihon-u.ac.jp>
2023-11-01 18:17:10 +0900

    react-tutlriaXのアップ

---
+++ react-tutorialX/src/App.jsx
@@ -0,0 +1,58 @@
+import { randomValueFromArray } from "./random";
+import React, { useState } from 'react';
+
+export default function App() {
+  const [showStory, setShowStory] = useState(false);
+  const [xItem, setXItem] = useState("");
+  const [yItem, setYItem] = useState("");
+  const [zItem, setZItem] = useState("");
+  const [ukus, setUkus] = useState("us");
+  const [f, setF] = useState("94");
+  const [Player, setPlayer] = useState("Bob")
+
+  const generateStory = () => {
+    if (showStory) {
+      setXItem(randomValueFromArray(["Willy the Goblin", "Big Daddy", "Father Christmas"]));
+      setYItem(randomValueFromArray(["the soup kitchen", "Disneyland", "the White House"]));
+      setZItem(randomValueFromArray(["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]));
+    }
+
+    if (ukus === "uk") {
+      setF("34");
+    }
+    if(ukus === "us"){
+      setF("94")
+    }
+    setShowStory(true);
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="customname">Enter custom name:</label>
+        <input type="text" placeholder="Player" value={Player} onChange={(e) => setPlayer(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="us">
+          <input type="radio" name="u" value="us" checked={ukus === "us"} onChange={() => setUkus("us")} />US
+        </label>
+        <label htmlFor="uk">
+          <input type="radio" name="u" value="uk" checked={ukus === "uk"} onChange={() => setUkus("uk")} />UK
+        </label>
+      </div>
+
+      <div>
+        <button onClick={generateStory}>Generate random story</button>
+      </div>
+
+      {showStory && (
+        <p>
+          It was {f} fahrenheit outside, so {xItem} went for a walk. When they
+          got to {yItem}, they stared in horror for a few moments, then {zItem}.
+          {Player} saw the whole thing, but was not surprised — {xItem} weighs 300
+          pounds, and it was a hot day.
+        </p>
+      )}
+    </>
+  );
+}
